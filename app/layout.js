d09973f9de44f848012d71a728d930c6883a4bfd
Yogesh Pal <yogeshpal5049@gmail.com>
2025-11-02 23:14:09 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import Header from "@/components/Header";
+import "./globals.css";
+import { TaskProvider } from "@/providers/TaskProvider";
+
+export const metadata = {
+  title: "Task Manager",
+  description: "Manage your tasks efficiently",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-50 text-gray-900 min-h-screen">
+        {/* <TaskProvider> */}
+          <Header />
+          {children}
+        {/* </TaskProvider> */}
+      </body>
+    </html>
+  );
+}
