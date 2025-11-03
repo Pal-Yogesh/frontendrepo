3a88f5958f1aa31c6ae2f5c123c61135efe48ac8
Yogesh Pal <yogeshpal5049@gmail.com>
2025-11-03 17:12:13 +0530

    done

--- app/page.js
+++ app/page.js
@@ -8,9 +8,9 @@ export default function page() {
 
 
 
-  
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-50 to-gray-200 text-gray-800 px-4">
+    <div className="flex flex-col items-center justify-center min-h-screen text-gray-800 px-4">
       {/* <div className="bg-white rounded-2xl shadow-lg p-8 max-w-md w-full text-center">
         <h1 className="text-3xl font-semibold mb-4">Access Restricted</h1>
         <p className="text-gray-600 mb-6">
