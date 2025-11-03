fa0d3a3646394279d2a90b6fdefca57b11ca1139
Yogesh Pal <yogeshpal5049@gmail.com>
2025-11-03 17:07:26 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
+import Login from "./login/page";
 
 export default function page() {
   const router = useRouter();
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-gray-50 to-gray-200 text-gray-800 px-4">
-      <div className="bg-white rounded-2xl shadow-lg p-8 max-w-md w-full text-center">
+      {/* <div className="bg-white rounded-2xl shadow-lg p-8 max-w-md w-full text-center">
         <h1 className="text-3xl font-semibold mb-4">Access Restricted</h1>
         <p className="text-gray-600 mb-6">
           You need to log in to access this page. Please sign in to continue.
@@ -19,7 +20,8 @@ export default function page() {
         >
           Go to Login
         </button>
-      </div>
+      </div> */}
+      <Login />
     </div>
   );
 }
